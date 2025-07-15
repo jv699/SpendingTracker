352e44d6d418af2c0d8d9b00222c2f03d03aaeaa
Josh Vacarchuk <jvacarch@gmail.com>
2025-07-15 19:48:32 -0400

    init

---
+++ SpendingTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpendingTracker
+//
+//  Created by Josh Vacarchuk on 7/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpendingTracker/SpendingTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendingTrackerApp.swift
+//  SpendingTracker
+//
+//  Created by Josh Vacarchuk on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SpendingTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
